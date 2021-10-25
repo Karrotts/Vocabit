65012ed498e678a0feaa7e6ad0551f1eda6c4883
wes <Wesleymiller1996@gmail.com>
2021-10-25 16:22:56 -0400

    added database docs

---
+++ Vocabit/Controllers/RandomController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vocabit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomController : Controller
+    {
+        [HttpGet("/api/random")]
+        public string Get()
+        {
+            return "beans";
+        }
+
+        [HttpGet("/api/random/{amount}")]
+        public string GetAmount(int amount)
+        {
+            return amount.ToString();
+        }
+    }
+}
